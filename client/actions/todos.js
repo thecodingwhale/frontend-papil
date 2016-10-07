7db79c4b2500729a94c1c400c1ddf9e5a05f422a
Aldren Terante <aldren.terante@gmail.com>
2016-10-08 01:35:50 +0800

    add basic crud for frontend

--- client/actions/todos.js
+++ client/actions/todos.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAction } from 'redux-actions'
 
 export function fetchTodo() {
-  const request = axios.get('http://pet-project.local/task/all')
+  const request = axios.get('http://papil.local/task/all')
   return (dispatch) => {
     request.then((response) => {
       dispatch({
@@ -14,9 +14,57 @@ export function fetchTodo() {
 }
 
 // export const fetchTodo = createAction('fetch todo')
-export const addTodo = createAction('add todo')
-export const deleteTodo = createAction('delete todo')
-export const editTodo = createAction('edit todo')
-export const completeTodo = createAction('complete todo')
+export function addTodo(todo) {
+  const request = axios.post('http://papil.local/task', {
+    user_id: 1,
+    text: todo,
+    completed: 0
+  });
+  return (dispatch) => {
+    request.then((response) => {
+      dispatch({
+        type: 'add todo',
+        payload: todo
+      })
+    })
+  }
+}
+
+export function deleteTodo(id) {
+  const request = axios.delete('http://papil.local/task/' + id);
+  return (dispatch) => {
+    dispatch({
+      type: 'delete todo',
+      payload: id
+    })
+  }
+}
+
+export function editTodo(todo) {
+  const request = axios.patch('http://papil.local/task/' + todo.id, {
+    user_id: 1,
+    text: todo.text
+  });
+  return (dispatch) => {
+    dispatch({
+      type: 'edit todo',
+      payload: todo
+    })
+  }
+}
+
+export function completeTodo(todoId) {
+  const request = axios.patch('http://papil.local/task/' + todoId, {
+    user_id: 1,
+    completed: true
+  });
+  return (dispatch) => {
+    dispatch({
+      type: 'complete todo',
+      payload: todoId
+    })
+  }
+}
+
 export const completeAll = createAction('complete all')
 export const clearCompleted = createAction('clear complete')
